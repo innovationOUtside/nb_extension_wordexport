2429012bd5fea49eb3dcac811854ca65a09eb181
Tony Hirst <tony.hirst@gmail.com>
2018-01-18 15:40:44 +0000

    First commit of extension as used in TM351_17J

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='wordexport',
+      version='0.0.1',
+      description='Export Jupyter notebook as .docx file',
+      author='Tony Hirst',
+      author_email='tony.hirst@open.ac.uk',
+      license='MIT',
+      packages=['wordexport'],
+      zip_safe=False)
---
+++ wordexport/wordexport.py
@@ -0,0 +1,72 @@
+# Copyright (c) The Open University, 2017
+# Copyright (c) Jupyter Development Team.
+
+# Distributed under the terms of the Modified BSD License.
+
+# Custom bundler extensions API:
+# http://jupyter-notebook.readthedocs.io/en/latest/extending/bundler_extensions.html
+# Requires: _jupyter_bundlerextension_paths(), bundle()
+# Based on: https://github.com/jupyter-incubator/dashboards_bundlers/
+
+import os
+import subprocess
+import shlex
+import shutil
+import tempfile
+
+def _jupyter_bundlerextension_paths():
+    '''API for notebook bundler installation on notebook 5.0+'''
+    return [{
+                'name': 'wordexport_wordexport',
+                'label': 'MS Word (.docx)',
+                'module_name': 'wordexport.wordexport',
+                'group': 'download'
+            }]
+
+def bundle(handler, model):
+	'''
+	Downloads a notebook as a Microsoft Word .docx document after converting to HTML
+	'''
+	
+	# Based on https://github.com/jupyter-incubator/dashboards_bundlers
+	
+	abs_nb_path = os.path.join(
+		handler.settings['contents_manager'].root_dir,
+		model['path']
+	)
+		
+	notebook_basename = os.path.basename(abs_nb_path)
+	notebook_name = os.path.splitext(notebook_basename)[0]
+	
+	tmp_dir = tempfile.mkdtemp()
+	
+	# Generate HTML version of file
+	cmd='jupyter nbconvert --to html "{abs_nb_path}" --output-dir "{tmp_dir}"'.format(abs_nb_path=abs_nb_path,tmp_dir=tmp_dir)
+	#os.system(cmd)
+	subprocess.check_call(cmd, shell=True)
+	
+	staged=os.path.join(tmp_dir, notebook_name)
+	# Convert to MS Word .docx
+	cmd='pandoc -s "{staged}.html" -o "{staged}.docx"'.format(staged=staged)
+	#os.system(cmd)
+	subprocess.check_call(cmd, shell=True)
+		
+	handler.set_header('Content-Disposition', 'attachment; filename="%s"' % (notebook_name + '.docx'))
+	
+	handler.set_header('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+	
+	with open("{staged}.docx".format(staged=staged), 'rb') as bundle_file:
+		handler.write(bundle_file.read())
+
+	handler.finish()
+
+
+	# We read and send synchronously, so we can clean up safely after finish
+	shutil.rmtree(tmp_dir, True)
+		
+        
+        
+        
+        
+        
+        
