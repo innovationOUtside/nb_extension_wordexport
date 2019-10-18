1875bb04c823b8601b713b540a9cb7febae2c79e
Tony Hirst <tony.hirst@gmail.com>
2019-10-18 17:04:31 +0100

    I need to learn how to handle version numbers properly...

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='wordexport',
-      version='0.0.1',
+      version='0.0.2',
       description='Export Jupyter notebook as .docx file',
       author='Tony Hirst',
       author_email='tony.hirst@open.ac.uk',
